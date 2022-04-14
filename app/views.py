bea5e27cd6da79e1e5042aa4af4c4b07cbf45736
aniqp <aniqpremji@gmail.com>
2022-04-14 00:56:40 -0400

    added flash messages to sign-up page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, flash
 from app.functions.todays_games import *
 from app.functions.get_team_credentials import *
 from app.functions.get_best_player import *
@@ -46,6 +46,35 @@ def games(game_id):
         away_team = away_team
     )
 
-@app.route("/sign-up")
+@app.route("/sign-up", methods = ['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email length must be greater than 3 characters.', category = 'error')
+        elif len(firstName) < 2:
+            flash('First name length must be greater than 1 character.', category = 'error')
+        elif len(lastName) < 2:
+            flash('Last name length must be greater than 1 character.', category = 'error')           
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category = 'error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category = 'error')
+        else:
+            flash('Account created!', category='success')
+
+
     return render_template("sign_up.html")
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    return "<p>Logout</p>"
