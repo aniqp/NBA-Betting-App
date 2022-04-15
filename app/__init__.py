9c38f7cc9afe411c2c4ee99fae1202c2cda4ce21
aniqp <aniqpremji@gmail.com>
2022-04-15 00:51:05 -0400

    added user login portal

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,39 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 app = Flask(__name__)
 app.secret_key = "v5mqvgwj"
+# tell program where DB is located
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# define app to use with database
+db.init_app(app)
 
 from app import views
+
+# import models file to define class before we initialize the database
+from .models import User, Bet
+
+def create_database(app):
+    if not path.exists('app/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+
+create_database(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+
+# by default, look for primary key to specify a user upon login
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 from app import app
-from flask import render_template, request, flash
+from . import db
+from flask import render_template, request, flash, redirect, url_for
+from flask_login import login_user, logout_user, login_required, current_user
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.functions.todays_games import *
 from app.functions.get_team_credentials import *
 from app.functions.get_best_player import *
 
 @app.route("/")
+@login_required
 def index():
     game = todays_games()
     games = {}
@@ -14,7 +19,7 @@ def index():
             'home_team': get_team_name(value['home_team'])
         } })
 
-    return render_template("index.html", games = games)
+    return render_template("index.html", games = games, user = current_user)
 
 
 @app.route("/games/<string:game_id>/")
@@ -50,31 +55,59 @@ def games(game_id):
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email already exists.', category = 'error')
+        elif len(email) < 4:
             flash('Email length must be greater than 3 characters.', category = 'error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name length must be greater than 1 character.', category = 'error')
-        elif len(lastName) < 2:
+        elif len(last_name) < 2:
             flash('Last name length must be greater than 1 character.', category = 'error')           
         elif password1 != password2:
             flash('Passwords don\'t match.', category = 'error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category = 'error')
         else:
+            new_user = User(email=email, first_name= first_name, last_name=last_name, password=generate_password_hash(password1, method='sha256'))
+            db.create_all()
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember = True)
             flash('Account created!', category='success')
+            return redirect(url_for('index'))
 
-
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # first returns the first result
+        user = User.query.filter_by(email=email).first()
+        # if a user is found, check if password they typed in exists in the database
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category = 'success')
+                login_user(user, remember = True)
+                return redirect(url_for('index'))
+            else:
+                flash('Incorrect password, try again.', category = 'error')
+        else:
+            flash('Email does not exist.', category = 'error')
+        
+    return render_template("login.html", user = current_user)
 
 @app.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('login'))
