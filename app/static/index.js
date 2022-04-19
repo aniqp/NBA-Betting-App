d75198425f3276cfd6e17b8bc6405c22aa45e43f
aniqp <aniqpremji@gmail.com>
2022-04-19 01:33:56 -0400

    fixed delete item bug, functional now

--- app/static/index.js
+++ app/static/index.js
@@ -1,7 +1,7 @@
 function deleteBet(betID) {
     fetch("/delete-bet", {
         method: "POST",
-        body: JSON.stringify({ betID: betID})
+        body: JSON.stringify({betID: betID})
     }).then((_res) => {
         window.location.href="/my-bets";
     });
