07c32f44bea8460facb37d70a64bb708d0343053
aniqp <aniqpremji@gmail.com>
2022-04-26 21:42:54 -0400

    change

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 app = Flask(__name__)
 app.secret_key = "v5mqvgwj"
 # tell program where DB is located
-# app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+## app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sfsogljfwisbci:743b63e81beb244453fdfce955b09c46199aa741b02dff683cb31f396fbdca7c@ec2-3-223-213-207.compute-1.amazonaws.com:5432/d4q7apck9l80a8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # define app to use with database
